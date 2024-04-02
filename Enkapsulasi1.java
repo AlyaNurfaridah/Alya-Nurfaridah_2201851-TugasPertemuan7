e91bfcfc90318740581ce0fd88263360a580e77e
AlyaNurfaridah <150658024+AlyaNurfaridah@users.noreply.github.com>
2024-04-02 22:27:47 +0700

    LinearEquation.java

--- Enkapsulasi1.java
+++ Enkapsulasi1.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.enkapsulasi1;
+package com.mycompany.LinearEquation;
 
 /**
  *
@@ -10,46 +10,76 @@ package com.mycompany.enkapsulasi1;
  */
 import java.util.Scanner;
 
-public class Enkapsulasi1 {
+public class LinearEquation {
     private double a, b, c, d, e, f;
 
-    public double getA() { return a; }
-    public double getB() { return b; }
-    public double getC() { return c; }
-    public double getD() { return d; }
-    public double getE() { return e; }
-    public double getF() { return f; }
+    public LinearEquation() {
+    }
 
-    public boolean isSolvable() { return (a * d - b * c) != 0; }
+    public LinearEquation(double a, double b, double c, double d, double e, double f) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+    }
 
-    public double getX() { return (e * d - b * f) / (a * d - b * c); }
-    public double getY() { return (a * f - e * c) / (a * d - b * c); }
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getD() {
+        return d;
+    }
+
+    public double getE() {
+        return e;
+    }
+
+    public double getF() {
+        return f;
+    }
+
+    public boolean penyelesaian() {
+        return (a * d - b * c) != 0;
+    }
+
+    public double getX() {
+        return (e * d - b * f) / (a * d - b * c);
+    }
+
+    public double getY() {
+        return (a * f - e * c) / (a * d - b * c);
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Masukkan nilai a:");
+        
+        System.out.print("Masukkan nilai a: ");
         double a = scanner.nextDouble();
-        System.out.println("Masukkan nilai b:");
+        System.out.print("Masukkan nilai b: ");
         double b = scanner.nextDouble();
-        System.out.println("Masukkan nilai c:");
+        System.out.print("Masukkan nilai c: ");
         double c = scanner.nextDouble();
-        System.out.println("Masukkan nilai d:");
+        System.out.print("Masukkan nilai d: ");
         double d = scanner.nextDouble();
-        System.out.println("Masukkan nilai e:");
+        System.out.print("Masukkan nilai e: ");
         double e = scanner.nextDouble();
-        System.out.println("Masukkan nilai f:");
+        System.out.print("Masukkan nilai f: ");
         double f = scanner.nextDouble();
 
-        Enkapsulasi1 equation = new Enkapsulasi1();
-        equation.a = a;
-        equation.b = b;
-        equation.c = c;
-        equation.d = d;
-        equation.e = e;
-        equation.f = f;
+        LinearEquation equation = new LinearEquation(a, b, c, d, e, f);
 
-        if (equation.isSolvable()) {
+        if (equation.penyelesaian()) {
             System.out.println("Solusi untuk sistem persamaan tersebut adalah:");
             System.out.println("x = " + equation.getX());
             System.out.println("y = " + equation.getY());
@@ -58,4 +88,3 @@ public class Enkapsulasi1 {
         }
     }
 }
-
